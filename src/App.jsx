9444babf46cbe34cc21fa92bab83d1ab0f186c07
Shorabh Lather <yourgithubemail@gmail.com>
2026-03-06 09:46:30 +0530

    navbar is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+import Contactpage from "./pages/Contactpage";
 import "./App.css";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<Contactpage />} />
       </Routes>
     </>
   );
---
+++ src/pages/Contactpage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Contactpage = () => {
+  return <section>
+    <Navbar/>
+  </section>;
+};
+
+export default Contactpage;
